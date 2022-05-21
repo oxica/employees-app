d76097ddf599d15215a57e08a2325464824d57ff
Oksana <kskaplun@gmail.com>
2022-05-21 09:42:00 +0300

    fix

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -5,7 +5,13 @@ import "./employees-list.css";
 const EmployeesList = ({ data }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
-    return <EmployeesListItem key={id} {...itemProps} />;
+    return (
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => console.log("Deleted")}
+      />
+    );
   });
 
   return <ul className="app-list list-group">{elements}</ul>;
