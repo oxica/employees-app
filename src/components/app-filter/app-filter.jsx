47a2fe9c7b5efd6e2975a9974e8188dd0843eedb
Oksana <kskaplun@gmail.com>
2022-05-21 14:22:13 +0300

    fix

--- src/components/app-filter/app-filter.jsx
+++ src/components/app-filter/app-filter.jsx
@@ -23,6 +23,7 @@ const AppFilter = (props) => {
   });
 
   return <div className="btn-group">{buttons}</div>;
+  // return <div className="btn-group">{buttons}</div>;
 };
 
 export default AppFilter;
